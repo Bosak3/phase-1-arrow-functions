1a19fd37c4e36bbb3ede3f9f0c847dab5f7219a0
Adam <bosak3@gmail.com>
2021-10-15 18:02:25 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,41 @@
+// function foo() {
+//     return 'bar';
+//     // console.log('bar');
+// }
+// foo()
+
+// const foo = function(){
+//     return 'bar';
+//     // console.log('bar')
+// }
+// foo()
+
+// const sum = (parameter1, parameter2) => {
+//     console.log(`Adding ${parameter1}`);
+//     console.log(`Adding ${parameter2}`);
+//     return parameter1 + parameter2;
+//   }
+//   sum(1,2); //=> 3
+
+
+    // .map() // method that takes a function, passes (iterates) each element of the array through the function
+    // adds the returned values to a new array. Leaving the original array unchanged.
+
+// const nums = [1,2,3];
+
+   // Uses arrow function
+// const squares = nums.map( x=> x ** 2);
+
+// squares;
+// nums;
+
+// console.log(squares)
+// console.log(nums)
+
+let add = (a, b) => a+ b;
+
+const divide = function (num1, num2){
+    return (2000 / 100);
+}
+
+square = parameter1 => parameter1 * parameter1;
